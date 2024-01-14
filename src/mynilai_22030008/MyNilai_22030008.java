08cf7d74518a02c1981b846e273a3695306bb84b
Na8888 <rohanaadilia@gmail.com>
2024-01-14 15:48:15 +0700

    first commit

---
+++ src/mynilai_22030008/MyNilai_22030008.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mynilai_22030008;
+
+/**
+ *
+ * @author ASUS Vivobook
+ */
+public class MyNilai_22030008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new UASnana().setVisible(true);
+    }
+    
+}
